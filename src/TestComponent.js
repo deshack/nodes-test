528874b2c5de07582f008714f5029a64378c326e
Mattia Migliorini <deshack@ubuntu.com>
2018-12-20 17:07:59 +0100

    Create TestComponent.js

---
+++ src/TestComponent.js
@@ -0,0 +1,20 @@
+import {Component, Input, Output, imageSocket} from '/dist/js/modules.js';
+
+export default class TestComponent extends Component {
+  constructor() {
+    super('Test');
+  }
+  
+  builder(node) {
+    const input = new Input('image', 'Image', imageSocket);
+    const output = new Output('image', 'Image', imageSocket);
+  
+    return node.addOutput(output);
+  }
+  
+  worker(node, inputs, outputs) {
+    if (typeof inputs['image'] !== 'undefined') {
+      outputs['image'] = inputs['image'];
+    }
+  }
+}
